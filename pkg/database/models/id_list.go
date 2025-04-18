fed572102bb88639b05be43f6d2495e30d44ba50
Eliáš Král <ilya.korolev@outlook.com>
2025-04-18 14:32:26 +0000

    fix: tags

--- pkg/database/models/id_list.go
+++ pkg/database/models/id_list.go
@@ -4,21 +4,34 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
+//nolint:recvcheck // it should impelement Valuer by value for some reasons
 type StringList []string
 
 // Implement the sql.Scanner interface
 func (s *StringList) Scan(value any) error {
-	bytes, ok := value.([]byte)
-	if !ok {
-		return fmt.Errorf("failed to unmarshal StringList value '%+v'", value)
+	str, ok := value.(string)
+	if ok {
+		if len(str) == 0 {
+			return nil
+		}
+
+		if str[0] != '[' {
+			*s = strings.Split(str, ",")
+			return nil
+		}
+
+		return json.Unmarshal([]byte(str), s)
 	}
 
-	result := []string{}
-	err := json.Unmarshal(bytes, &result)
-	*s = StringList(result)
-	return err
+	bytes, ok := value.([]byte)
+	if ok {
+		return json.Unmarshal(bytes, s)
+	}
+
+	return fmt.Errorf("failed to unmarshal StringList value '%+v'", value)
 }
 
 // Implement the driver.Valuer interface
